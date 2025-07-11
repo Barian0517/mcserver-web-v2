571ce183a466bdfa33e071ce092a7b82a7fdca2d
Yuan Chiu <chyuaner@gmail.com>
2025-07-11 21:13:28 +0800

    change config base

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://barian0517.github.io/mcserver-web-v2/',
+  base: 'mcserver-web-v2',
   output: 'static',
   redirects: {
     '/': '/youer',
