b2c24efdb89418e5e026d33cbefaad281e1f4a38
Yuan Chiu <chyuaner@gmail.com>
2025-07-11 23:08:48 +0800

    fix path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+const base = process.env.ASTRO_BASE || '/';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  base: process.env.ASTRO_BASE || '/',
-  trailingSlash: "never",
+  base,
+  trailingSlash: "ignore",
   redirects: {
-    '/': '/youer',
+    [base]: `${base}youer`,
   }
 });
