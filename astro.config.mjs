2fdcdebc6e4bf7012c97a3e71eb8be6d77d78c91
Yuan Chiu <chyuaner@gmail.com>
2025-07-11 22:49:03 +0800

    fix path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-const isProd = process.env.NODE_ENV === 'production';
-const base = isProd ? '/mcserver-web-v2/' : '/';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  base,
+  base: process.env.ASTRO_BASE || '/',
   trailingSlash: "never",
   redirects: {
     '/': '/youer',
