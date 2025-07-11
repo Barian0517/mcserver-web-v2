5080f61d66c44b9581297eeb20de98375fcc1b37
Yuan Chiu <chyuaner@gmail.com>
2025-07-11 23:08:48 +0800

    Revert "fix path"
    This reverts commit b2c24efdb89418e5e026d33cbefaad281e1f4a38.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-const base = process.env.ASTRO_BASE || '/';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  base,
-  trailingSlash: "ignore",
+  base: process.env.ASTRO_BASE || '/',
+  trailingSlash: "never",
   redirects: {
-    [base]: `${base}youer`,
+    '/': '/youer',
   }
 });
