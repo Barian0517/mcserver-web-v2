6cc08243e1c9cfb2a056f9e864b872c7b4e61e46
Yuan Chiu <chyuaner@gmail.com>
2025-07-20 19:11:25 +0800

    Revert "修正base"
    This reverts commit e2396f18ab61c394f75e2a2062aee3e250f84a99.
    
    Conflicts:
    	astro.config.mjs
    	src/pages/index.html

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-const isProd = process.env.NODE_ENV === 'production';
-const base = isProd ? '/mcserver-web-v2/' : '/';
-
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+<<<<<<< HEAD
   base,
   trailingSlash: "never",
   redirects: {
     '/': '/youer',
   }
+=======
+  redirects: {
+    '/': '/mcserver-web-v2/youer',
+  },
+>>>>>>> parent of e2396f1... 修正base
 });
